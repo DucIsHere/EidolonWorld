a1ebc342ae6c832fab3d3fd572d763c02d96ce02
DucIsHere <thelegendwolf07@gmail.com>
2025-10-26 12:27:32 +0700

    Update MixinNoiseRouterData.java

--- common/src/main/java/com/eidolonworld/mixin/MixinNoiseRouterData.java
+++ common/src/main/java/com/eidolonworld/mixin/MixinNoiseRouterData.java
@@ -16,6 +16,10 @@ public abstract class MixinNoiseRouterData {
 
     @Inject(method = "overworld", at = @At("RETURN"), cancellable = true)
     private static void injectAmplify(HolderGetter<DensityFunction> densityGetter, HolderGetter<NormalNoise.NoiseParameters> noiseGetter, CallbackInfoReturnable<NoiseRouter> cir) {
+        DensityFuction base = DensityFunctions.OVERWORLD_FINAL_DENSITY;
+        AmplifyNoise amplify = new AmplifyNoise(base, 4.5, 0.002, 15000);
+        NoiseRouter router = ((RandomStateAccessor) randomState).getRouter();
+        ((NoiseRouterAccessor) router).setFinalDensity(amplify);
         NoiseRouter original = cir.getReturnValue();
         DensityFunction amplified = new AmplifyNoise(original.finalDensity(), 4.0);
         NoiseRouter newRouter = new NoiseRouter(
