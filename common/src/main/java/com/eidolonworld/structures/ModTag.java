15684ead289328037b7c04839c2fe16ede812fb1
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-18 11:29:24 +0700

    Update ModTag.java

--- common/src/main/java/com/eidolonworld/structures/ModTag.java
+++ common/src/main/java/com/eidolonworld/structures/ModTag.java
@@ -4,5 +4,6 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biomes;
 
 public class ModTag {
-    public static final TagKey<Biomes> MY_BIOME_TAG = TagKey.create(Biomes.REGISTRY, new ResourceLocation("eidolonworld", "shadowed_grove"));
+    public static final TagKey<Biomes> SHADOWED_GROVE =
+    TagKey.create(Biomes.REGISTRY, new ResourceLocation("eidolonworld", "shadowed_grove"));
 }
